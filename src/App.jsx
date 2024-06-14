3e4610e0ef7e5d27f6cd8c62ded01ce976b54c2e
kaileychavez165 <kachavez165@gmail.com>
2024-06-14 15:33:19 -0700

    Complete milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import .meta.env.VITE_API_KEY
 
 const App = () => {
-  <div className="App">
-    
-  </div>
+  return (
+    <div className="App">
+      <h1>Flixster</h1>
+    </div> 
+  )
 }
 
 export default App
